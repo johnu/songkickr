927126a83340c565c056155e781e07ac1fb55dee
Tommy Poulter <tppoulter@yahoo.com>
2011-12-04 17:31:15 -0600

    added some additional API supported fields to the venue object.

--- lib/songkickr/venue.rb
+++ lib/songkickr/venue.rb
@@ -1,12 +1,13 @@
 module Songkickr
   class Venue
-    attr_accessor :display_name, :id, :lat, :lng
+    FIELDS = %w{display_name id lat lng street zip phone website uri capacity description} # city metro_area
+
+    FIELDS.map { |f| attr_accessor f.to_sym }
     
     def initialize(venue_hash)
-      @display_name = venue_hash["displayName"]
-      @id           = venue_hash["id"]
-      @lat          = venue_hash["lat"]
-      @lng          = venue_hash["lng"]
+      FIELDS.each do |f|
+        instance_variable_set("@#{f}", venue_hash[f.gsub(/_([a-z])/){$1.upcase}])
+      end
     end
   end
 end
